add9b8dd634e4ac8257b6e8b281836892a09dd13
KSHITIJ DINESH NIRDHAR <nirdharkshitij2006@gmail.com>
2026-05-09 14:41:19 +0530

    Update prob6_zigzag_conv.py

--- strings/prob6_zigzag_conv.py
+++ strings/prob6_zigzag_conv.py
@@ -1,1 +1,21 @@
+class Solution:
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
 
+        rows = [""] * numRows
+        cur_row = 0
+        going_down = False
+
+        for char in s:
+            rows[cur_row] += char
+
+            if cur_row == 0 or cur_row == numRows - 1:
+                going_down = not going_down
+
+            if going_down:
+                cur_row += 1
+            else:
+                cur_row -= 1
+
+        return "".join(rows)
