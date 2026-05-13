4835784227f392c7ed729cb534eed8eeb657a5e0
KSHITIJ DINESH NIRDHAR <nirdharkshitij2006@gmail.com>
2026-05-13 16:54:47 +0530

    Add regular expression matching solution
    Implement a recursive solution with memoization for regular expression matching.

--- dynamic programming/prob23_regular expression.py
+++ dynamic programming/prob23_regular expression.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def isMatch(self, s, p):
+        dp = {}
+        def dfs(i, j):
+            if (i, j) in dp:
+                return dp[(i, j)]
+            if j == len(p):
+                return i == len(s)
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+            if j + 1 < len(p) and p[j + 1] == "*":
+                dp[(i, j)] = dfs(i, j + 2) or (match and dfs(i + 1, j))
+                return dp[(i, j)]
+            if match:
+                dp[(i, j)] = dfs(i + 1, j + 1)
+                return dp[(i, j)]
+            dp[(i, j)] = False
+            return False
+        return dfs(0, 0)
